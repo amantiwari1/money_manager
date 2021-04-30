d95857bdb5bd7b225184ba00da379b3823496ee9
aman tiwari <amantiwari78632@gmail.com>
2021-04-30 23:59:25 +0530

    chagned tw

--- client/src/website/pages/Home.tsx
+++ client/src/website/pages/Home.tsx
@@ -1,7 +1,8 @@
+import tw from 'twin.macro'
 const Home = () => {
     return (
-        <div className="flex justify-center items-center" >
-            <h3 className="text-5xl font-bold" >Edit `client\src\website\pages\Home.tsx`</h3>
+        <div tw="flex justify-center items-center" >
+            <h3 tw="text-xl font-bold" >Edit `client\src\website\pages\Home.tsx`</h3>
         </div>
     )
 }
