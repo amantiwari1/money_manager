3d9e08871b8255660b5264874f1b5b484185e094
aman tiwari <amantiwari78632@gmail.com>
2021-04-30 23:59:14 +0530

    create sidebar

---
+++ client/src/website/components/Navbar/Narbar.tsx
@@ -0,0 +1,16 @@
+import tw from 'twin.macro'
+import Toggle from '../../theme/themeToggle'
+
+const Narbar = () => {
+  return (
+    <>
+      <div tw=" bottom-0	w-full h-20 md:(w-20 min-h-screen) bg-black fixed " >
+
+        <Toggle />
+
+      </div>
+    </>
+  )
+}
+
+export default Narbar
