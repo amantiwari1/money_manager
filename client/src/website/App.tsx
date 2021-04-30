3d6636c16258053826ac7ad1f035b5683e671ab5
aman tiwari <amantiwari78632@gmail.com>
2021-04-30 23:59:03 +0530

    added some components

--- client/src/website/App.tsx
+++ client/src/website/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from './theme/themeContext';
 import { Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
-import Toggle from './theme/themeToggle'
-
+import GlobalStyles from './theme/GlobalStyles';
+import Narbar from './components/Navbar/Narbar';
+import tw from 'twin.macro'
 
 function App() {
   return (
     <ThemeProvider>
-      <Toggle />
-      <Switch>
-        <Route exact path="/"  component={Home}  />
-      </Switch>
+      <GlobalStyles />
+      <Narbar />
+      <div tw="md:ml-20">
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
     </ThemeProvider>
   );
 }
