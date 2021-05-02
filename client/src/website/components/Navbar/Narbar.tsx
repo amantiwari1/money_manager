c9176b1b74c652b87c334bd0fb84d5a45ba42771
aman tiwari <amantiwari78632@gmail.com>
2021-05-02 23:03:00 +0530

    added fully style responsive

--- client/src/website/components/Navbar/Narbar.tsx
+++ client/src/website/components/Navbar/Narbar.tsx
@@ -1,47 +1,67 @@
+import { useRef, useState } from 'react'
 import tw, { styled } from 'twin.macro'
 import Toggle from '../../theme/themeToggle'
+import { RiMenu3Fill, RiCloseLine } from "react-icons/ri"
+import { useDetectOutsideClick } from '../../utils/useDetectOutsideClick'
 
-const Icon = tw.div`h-8 w-8 bg-white rounded-full cursor-pointer`
+const Icon = tw.div`h-8 w-8 bg-red-300 rounded-full cursor-pointer  `
 
 const Brand = tw.h1`text-2xl font-bold flex text-center text-white`
 
+interface Props {
+  isOpen: boolean;
+}
 
 const NavbarItem = styled.div`
-    ${tw`min-h-screen pt-4 dark:bg-black fixed   shadow-2xl bg-gray-400 `}
-  
-
+    left: ${(props: Props) => props.isOpen ? "0" : "-100%"};
+    ${tw`h-screen pt-4 top-0  md:left-0 dark:bg-black fixed   shadow-2xl bg-gray-500 w-48 md:w-16 z-10`}
+    transition: width 0.3s ease-in-out, left 0.3s ease-in-out;
     
-
+    &:hover {
+     ${tw`w-48`}
+    }
 
     p {
-      ${tw`hidden`}
+      ${tw`md:hidden`}
+      animation: fadeIn linear 0.3s; 
     }
 
     &:hover {
       p {
-        ${tw`block`}
+        ${tw`md:block`}
       }
     }
- 
+
+    @keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
 `
 const Narbar = () => {
+
+  const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useDetectOutsideClick(dropdownRef, false); useState(false)
   return (
     <>
-      <NavbarItem>
-        <div tw="flex flex-col md:space-y-96 items-center" >
+      <NavbarItem isOpen={isOpen} ref={dropdownRef} >
+        <div tw="flex flex-col space-y-10 md:space-y-96 items-center" >
 
-          <div tw="flex flex-col m-5  items-center  justify-between space-y-14" >
-            <Brand>MM</Brand>
+          <div tw="flex flex-col  items-center  justify-between space-y-14" >
 
-            <div tw="flex space-x-5 cursor-pointer" >
+            <div tw="flex items-center text-white space-x-9" >
+              <Brand>MM</Brand>
+              <RiCloseLine tw="md:hidden text-2xl" onClick={() => setIsOpen(!isOpen)} >X</RiCloseLine>
+            </div>
+
+            <div tw="flex space-x-5 cursor-pointer filter grayscale text-red-300 hover:(grayscale-0)" >
               <Icon />
               <p>Hello</p>
             </div>
-            <div tw="flex space-x-5 cursor-pointer" >
+            <div tw="flex space-x-5 cursor-pointer filter grayscale text-red-300 hover:(grayscale-0)" >
               <Icon />
               <p>Hello</p>
             </div>
-            <div tw="flex space-x-5 cursor-pointer" >
+            <div tw="flex space-x-5 cursor-pointer filter grayscale text-red-300 hover:(grayscale-0)" >
               <Icon />
               <p>Hello</p>
             </div>
@@ -58,6 +78,12 @@ const Narbar = () => {
 
 
       </NavbarItem>
+
+      <div tw="flex items-center w-full bg-red-300 h-14 shadow-md">
+
+        <RiMenu3Fill tw="text-2xl ml-4" onClick={() => setIsOpen(!isOpen)} >Menu</RiMenu3Fill>
+
+      </div>
     </>
   )
 }
