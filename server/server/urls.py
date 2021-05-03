c230e1387044bb78df2e39bd1a0328e4b80b22d4
aman tiwari <amantiwari78632@gmail.com>
2021-05-03 23:52:04 +0530

    added more urls and file

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('users.urls')),
+    path('api/v1/data/', include('incomes.urls')),
 ]
