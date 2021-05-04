f2b8078b3811ea2b520c40e8270e16d4c7710b9f
aman tiwari <amantiwari78632@gmail.com>
2021-05-04 17:39:30 +0530

    added more routes

--- client/src/website/App.tsx
+++ client/src/website/App.tsx
@@ -5,6 +5,8 @@ import GlobalStyles from './theme/GlobalStyles';
 import Navbar from './components/Navbar/Navbar';
 // eslint-disable-next-line
 import tw from 'twin.macro'
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <div tw="md:ml-20">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       </div>
     </ThemeProvider>
