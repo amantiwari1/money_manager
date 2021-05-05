6812a13615db300787c9a95b72819a23d984ead4
aman tiwari <amantiwari78632@gmail.com>
2021-05-05 22:32:36 +0530

    remove urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,5 +5,4 @@ from django.urls import path, include
 urlpatterns = [
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('users.urls')),
-    path('api/v1/data/', include('incomes.urls')),
 ]
